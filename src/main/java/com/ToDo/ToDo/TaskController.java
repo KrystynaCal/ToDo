14a3d745b538de446d380af5b5ed91c71bf482e3
KrystynaCal <krystynacal5@gmail.com>
2024-04-08 11:19:01 +0200

    first commit with some code

---
+++ src/main/java/com/ToDo/ToDo/TaskController.java
@@ -0,0 +1,25 @@
+package com.ToDo.ToDo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTask(){
+    List<Task> tasks = taskService.getAllTasks();
+    return new ResponseEntity<>(tasks, HttpStatus.OK);
+}
+}
---
+++ src/main/java/com/ToDo/ToDo/TaskService.java
@@ -0,0 +1,12 @@
+package com.ToDo.ToDo;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+    public TaskService(TaskRepository taskRepository) {this.taskRepository = taskRepository;}
+    public List<Task> getAllTasks() { return taskRepository.findAll(); }
+}
